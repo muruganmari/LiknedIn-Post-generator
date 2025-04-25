77fba51b0e915e50cedaa0dd193826efc145d466
muruganm <muruganmack98@gmail.com>
2025-04-24 20:15:13 -0700

    setting LLM

---
+++ llm_helper.py
@@ -0,0 +1,12 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+llm= ChatGroq(groq_api_key = os.getenv("GROQ_API_KEY"), model_name="compound-beta")
+
+if __name__ =="__main__":
+    response = llm.invoke("What are the two ingridient in briyani")
+    print(response.content)
